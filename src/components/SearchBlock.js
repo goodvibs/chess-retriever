f4a3c89d5b9b94f00fefcd8c1b13494b48abcd17
vibhavsingamshetty <vibsingamshetty@gmail.com>
2022-06-29 05:00:16 +0300

    Minor bug fixes and improvements

--- src/components/SearchBlock.js
+++ src/components/SearchBlock.js
@@ -4,7 +4,7 @@ import LogoSVG from './LogoSVG'
 
 export default function SearchBlock(props) {
     return (
-        <header className={(props.expanded ? 'translate-y-48 md:scale-110 lg:scale-125 xl:scale-150 ' : '') + 'flex flex-col items-center self-start rounded-xl bg-amber-300 text-teal-800 p-3 px-8 md:p-4 md:px-9 lg:p-5 lg:px-10 transition-all duration-500'}>
+        <header className={(props.expanded ? 'translate-y-48 md:scale-110 lg:scale-125 xl:scale-150 ' : '') + 'flex flex-col items-center rounded-xl bg-amber-300 text-teal-800 p-3 px-8 md:p-4 md:px-9 lg:p-5 lg:px-10 transition-all duration-500'}>
             <div className='flex items-center justify-center p-5 gap-4 my-auto'>
                 <LogoSVG className='w-10 fill-teal-800 object-cover md:w-12 lg:w-14 transition-all duration-500'/>
                 <h1 className='flex whitespace-nowrap child:text-4xl child:inline-flex child:whitespace-nowrap child:md:text-5xl child:lg:text-6xl'>
