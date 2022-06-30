ee873a9af3623a086206e6ee1672ccccd59144f9
vibhavsingamshetty <vibsingamshetty@gmail.com>
2022-06-30 17:34:14 +0300

    added a line of something

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import ReactGA from 'react-ga';
 
 const TRACKING_ID = 'UA-233253201-1';
 ReactGA.initialize(TRACKING_ID);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
